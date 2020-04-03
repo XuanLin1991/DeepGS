da9f38b6a8ae32ad3fd527e523f51bb6ea3195fd
Lin Xuan <44014504+jacklin18@users.noreply.github.com>
2020-04-03 00:06:32 -0500

    Update run_training.sh

--- code/run_training.sh
+++ code/run_training.sh
@@ -24,4 +24,4 @@ weight_decay=1e-5
 iteration=100
 
 setting=$DATASET--radius$radius--ngram$ngram--dim$dim--layer_gnn$layer_gnn--window$window--layer_cnn$layer_cnn--layer_output$layer_output--lr$lr--lr_decay$lr_decay--decay_interval$decay_interval--weight_decay$weight_decay--iteration$iteration
-python run_DeepGS2_new.py $DATASET $radius $ngram $dim $layer_gnn $window $layer_cnn $layer_output $lr $lr_decay $decay_interval $weight_decay $iteration $setting
+python run_DeepGS.py $DATASET $radius $ngram $dim $layer_gnn $window $layer_cnn $layer_output $lr $lr_decay $decay_interval $weight_decay $iteration $setting
